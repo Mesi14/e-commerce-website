8e53c0f4100a8e088ac400fd87ed7a133582e286
Mesi14 <Mesi18@proton.me>
2025-02-27 11:46:43 +0200

    :construction: add details page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
 import { BrowserRouter, Routes, Route } from "react-router";
 import PageNotFound from './pages/PageNotFound';
 
@@ -14,7 +15,7 @@ root.render(
     <Routes>
       <Route path="/" element={<App />}>
          <Route path="/" element={<Home />} />
-         {/* <Route path="/details/:id" element={<Details />} /> */}
+         <Route path="/details/:id" element={<ProductDetails />} /> 
          <Route path="*" element={<PageNotFound />} />
       </Route>
     </Routes>
