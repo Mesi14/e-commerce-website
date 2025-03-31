a27055c1a443f9173b3429996d527c7cfd8203ed
Mesi14 <Mesi18@proton.me>
2025-03-31 10:08:33 +0300

    :lipstick: style the hero section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,22 @@
 import '../styles/Product.css'
 import Product from '../components/Product';
+import { useState } from 'react';
 
 const Home = () => {
+  const [filterCat, setFilterCat] = useState('');
+
+  const getSelectedCat = () => {
+    return 
+  }
   return (
     <>
-      <div className="prods container-fluid">
-        <div className="container-fluid presentation d-flex text-white py-5">
-          <div className="row align-items-center">
-            <h1 className="mb-5 display-2"><strong>WearMe Clothes for everyone!</strong></h1>
-            <p>Mi magnis elementum nec eros at maximus? In iaculis dolor sit consequat per. Eu hac conubia viverra aliquam est. Odio felis elit in finibus cursus nulla torquent. Habitant netus varius; orci rutrum mattis lectus. Sit sagittis mus orci pulvinar, congue eu mauris. Phasellus facilisis vehicula aenean; libero velit platea vehicula. Ex turpis ex inceptos fusce maximus massa mus.</p>
-          </div>
-          <div className="col-md-6 text-center"> 
-            <img src="Assets/Images/demo_hoodie.png" className="img-fluid" alt="showroom" />                
-          </div>
+      <div className="prods container-fluid d-flex">
+        <div className="hero-text">
+          <h1 className=""><strong>WearMe Clothes for everyone!</strong></h1>
+          <p>Mi magnis elementum nec eros at maximus? In iaculis dolor sit consequat per. Eu hac conubia viverra aliquam est. Odio felis elit in finibus cursus nulla torquent. Habitant netus varius; orci rutrum mattis lectus. Sit sagittis mus orci pulvinar, congue eu mauris. Phasellus facilisis vehicula aenean; libero velit platea vehicula. Ex turpis ex inceptos fusce maximus massa mus.</p>
+        </div>
+        <div className="img-section"> 
+          <img src="Assets/Images/demo_hoodie.png" className="hero-img img-fluid" alt="showroom" />                
         </div>
       </div>
       <div className="container-fluid mt-2">
